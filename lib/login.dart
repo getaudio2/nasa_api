47d989c7a1fa2bff3d3ca0b2aa83ac736c6c88be
getaudio2 <paugermaisar@hotmail.es>
2022-02-17 17:07:20 +0100

    first commit

---
+++ lib/login.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const NasaApp());
+
+class NasaApp extends StatelessWidget {
+  const NasaApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    var title = 'Nasa api';
+    return MaterialApp(
+      title: title,
+      home: Scaffold(
+        body: SingleChildScrollView(
+          child: Stack(children: <Widget>[
+            Container(
+              width: double.infinity,
+              height: 250,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: <Color>[Colors.cyan, Colors.red],
+                ),
+              ),
+              child: Image.asset('assets/images/17_earth.png'),
+            ),
+            Container(
+              padding: const EdgeInsets.all(30.0),
+              margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 160),
+              width: double.infinity,
+              height: 300,
+              decoration: BoxDecoration(
+                color: Colors.white,
+              ),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "Usuari"),
+                  ),
+                  SizedBox(height: 20),
+                  TextFormField(
+                    decoration: const InputDecoration(labelText: "Contrasenya"),
+                    obscureText: true,
+                  ),
+                ],
+              ),
+            ),
+          ]),
+        ),
+      ),
+    );
+  }
+}
