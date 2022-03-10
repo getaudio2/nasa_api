bb50489edac28cc9c91e99bb100a024f8a69eb19
getaudio2 <paugermaisar@hotmail.es>
2022-03-10 17:03:40 +0100

    Created list of cards for apods

--- lib/screens/llistat.dart
+++ lib/screens/llistat.dart
@@ -14,7 +14,25 @@ class Llistat extends StatelessWidget {
       ),
       itemCount: apodList.length,
       itemBuilder: (context, index) {
-        return Image.network(apodList[index].url);
+        return Card(
+          child: InkWell(
+            splashColor: Colors.blue.withAlpha(30),
+            onTap: () {
+              debugPrint('Card tapped.');
+            },
+            /*child: const SizedBox(
+              width: 300,
+              height: 100,
+              child: Text('A card that can be tapped'),
+            ),*/
+            child: ListTile(
+              leading: Image.network(apodList[index].url),
+              title: Text(apodList[index].title),
+              subtitle: Text('Copyright: ' + apodList[index].copyright),
+            ),
+          ),
+        );
+        //return Image.network(apodList[index].url);
       },
     );
   }
